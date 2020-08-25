6d8fa0b1e79199f959cebad2493e3b3f1278a49f
Alexander Bußmann <alexander.bussmann@tum.de>
2020-08-25 13:26:33 +0200

    Change epsilon handling weno stencils

--- src/stencils/spatial_reconstruction_stencils/weno7.h
+++ src/stencils/spatial_reconstruction_stencils/weno7.h
@@ -176,34 +176,37 @@ class WENO7 : public Stencil<WENO7> {
       double const s13 = coef_smoothness_0_10_ * v3 + coef_smoothness_0_11_ * v4;
       double const s14 = coef_smoothness_0_13_ * v4;
 
-      double const s1 = ( v1 * s11 + v2 * s12 + v3 * s13 + v4 * s14 ) + epsilon_weno7_;
+      double const s1 = v1 * s11 + v2 * s12 + v3 * s13 + v4 * s14;
 
       double const s21 = coef_smoothness_1_01_ * v2 + coef_smoothness_1_02_ * v3 + coef_smoothness_1_03_ * v4 + coef_smoothness_1_04_ * v5;
       double const s22 = coef_smoothness_1_06_ * v3 + coef_smoothness_1_07_ * v4 + coef_smoothness_1_08_ * v5;
       double const s23 = coef_smoothness_1_10_ * v4 + coef_smoothness_1_11_ * v5;
       double const s24 = coef_smoothness_1_13_ * v5;
 
-      double const s2 = ( v2 * s21 + v3 * s22 + v4 * s23 + v5 * s24 ) + epsilon_weno7_;
+      double const s2 = v2 * s21 + v3 * s22 + v4 * s23 + v5 * s24;
 
       double const s31 = coef_smoothness_2_01_ * v3 + coef_smoothness_2_02_ * v4 + coef_smoothness_2_03_ * v5 + coef_smoothness_2_04_ * v6;
       double const s32 = coef_smoothness_2_06_ * v4 + coef_smoothness_2_07_ * v5 + coef_smoothness_2_08_ * v6;
       double const s33 = coef_smoothness_2_10_ * v5 + coef_smoothness_2_11_ * v6;
       double const s34 = coef_smoothness_2_13_ * v6;
 
-      double const s3 = ( v3 * s31 + v4 * s32 + v5 * s33 + v6 * s34 ) + epsilon_weno7_;
+      double const s3 = v3 * s31 + v4 * s32 + v5 * s33 + v6 * s34;
 
       double const s41 = coef_smoothness_3_01_ * v4 + coef_smoothness_3_02_ * v5 + coef_smoothness_3_03_ * v6 + coef_smoothness_3_04_ * v7;
       double const s42 = coef_smoothness_3_06_ * v5 + coef_smoothness_3_07_ * v6 + coef_smoothness_3_08_ * v7;
       double const s43 = coef_smoothness_3_10_ * v6 + coef_smoothness_3_11_ * v7;
       double const s44 = coef_smoothness_3_13_ * v7;
 
-      double const s4 = ( v4 * s41 + v5 * s42 + v6 * s43 + v7 * s44 ) + epsilon_weno7_;
+      double const s4 = v4 * s41 + v5 * s42 + v6 * s43 + v7 * s44;
 
       // Compute weights
-      double const a1 = coef_weights_1_ / ( s1 * s1 );
-      double const a2 = coef_weights_2_ / ( s2 * s2 );
-      double const a3 = coef_weights_3_ / ( s3 * s3 );
-      double const a4 = coef_weights_4_ / ( s4 * s4 );
+      // NOTE: The epsilon value is used here explicitly to avoid compiler optimizations when the epsilon is added directly to s_i.
+      //       This could lead to undesired behavior in case the values s1, ..., s4 are of similar magnitude.
+      //       Then, it cannot guaranteed anymore that a division by zero is avoided.
+      double const a1 = coef_weights_1_ / ( ( s1 + epsilon_weno7_ ) * ( s1 + epsilon_weno7_ ) );
+      double const a2 = coef_weights_2_ / ( ( s2 + epsilon_weno7_ ) * ( s2 + epsilon_weno7_ ) );
+      double const a3 = coef_weights_3_ / ( ( s3 + epsilon_weno7_ ) * ( s3 + epsilon_weno7_ ) );
+      double const a4 = coef_weights_4_ / ( ( s4 + epsilon_weno7_ ) * ( s4 + epsilon_weno7_ ) );
 
       double const one_a_sum = 1.0 / ( a1 + a2 + a3 + a4 );
 
