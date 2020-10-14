312e42301844c848299c5dbd5ee8ebd39ce3392a
Nils Hoppe <nils.hoppe@tum.de>
2020-10-14 19:31:49 +0200

    Removed wrong -r flag and wrong configuration file from merge-suite compilation script

--- testsuite/compile_merge_suite.sh
+++ testsuite/compile_merge_suite.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 python3.6 -m venv venv
 source venv/bin/activate
-pip3 install -r ../python
+pip3 install ../python
 
 source ./initialize_cluster.sh
-python3 ../python/scripts/run_testsuite.py ./Configuration/AerConfig_short.xml ../ --testsuite-name Testsuite_Merge_Compile
+python3 ../python/scripts/run_testsuite.py ./Configuration/.merge-compile.xml ../ --testsuite-name Testsuite_Merge_Compile
 
 deactivate
 rm -rf venv
