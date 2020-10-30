354fe6c376225bc5ed17d5d4ea98c85d45397116
Nils Hoppe <nils.hoppe@tum.de>
2020-10-30 11:01:07 +0100

    Improved compiler recognition in cmake and switched to GCC for cluster compilation.

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -1,17 +1,3 @@
-if( ALPACA_ENV STREQUAL "LRZ" )
-    MESSAGE( STATUS "Compile on LRZ" )
-    set( CMAKE_CXX_COMPILER "mpiCC" )
-elseif( ALPACA_ENV STREQUAL "AER" )
-    MESSAGE( STATUS "Compile on AER machine" )
-    set(CMAKE_CXX_COMPILER "/global/mpich-3.1/bin/mpic++" )
-elseif( ALPACA_ENV STREQUAL "SNG" )
-    message("-- Compile on SuperMUC NG")
-    set(CMAKE_CXX_COMPILER "mpicxx")
-else( ALPACA_ENV STREQUAL "LRZ" )
-    MESSAGE( STATUS "Compile with user default compilers" )
-    set( CMAKE_CXX_COMPILER "mpicxx" )
-endif( ALPACA_ENV STREQUAL "LRZ" )
-
 # Use gold linker, if it is installed and not disabled
 option( USEGOLD "Gold Linker" ON )
 if(UNIX AND NOT APPLE AND USEGOLD)
